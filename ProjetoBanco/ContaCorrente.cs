44f38f8d666ff7f335a2df9be70fbeadaf6d13a5
Kemy10 <120021911+Kemy10@users.noreply.github.com>
2023-02-02 11:58:53 -0300

    Add files via upload

--- ProjetoBanco/ContaCorrente.cs
+++ ProjetoBanco/ContaCorrente.cs
@@ -4,25 +4,58 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using ProjetoBanco;
 
 
 
-    public class ContaCorrente
+public class ContaCorrente
+{
+    public string nome;
+    public string conta;
+    public int agencia;
+    public double saldo;
+    public double valorTranferencia;
+
+    public bool Sacar(double valor)
     {
-         public string nome;
-        public string conta;
-       public  int agencia;
-       public double saldo;
+        if (this.saldo >= valor)
+        {
+            this.saldo -= valor;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Saldo " +
+                "insuficiente para realizar o saque.");
+            return false;
+        }
+    }
 
-    public void Depositar(double valor)
+        public void Depositar(double valor)
+        { 
+            this.saldo += valor;
+        }
+
+    public bool Transferir(double valor, ContaCorrente contaDestino)
     {
-       this.saldo += valor;
+
+        if (this.saldo >= valor)
+        {
+            valorTranferencia = valor;
+            this.saldo -= valor;
+            contaDestino.saldo += valor;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Não foi possível relizar a transferência. O saldo é insuficiente.");
+                return false;
+        }
+
+       
     }
 
-    public void sacar(double valor)
-    { this.saldo -= valor; 
-    }
-
-    }
+    
+}
 
 
